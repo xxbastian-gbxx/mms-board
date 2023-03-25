1bb45cdf0fa8d58c6bfb74499a63a087cdaa9d60
gb <sg2322007@gmail.com>
2023-03-25 17:37:50 +0100

    Fries Menu Button Aniamtion & Design

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,15 @@ import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
 </script>
 
+<style src="/src/assets/style-global.css"></style>
+
 <template>
     <Navbar/>
     <RouterView></RouterView>
 </template>
+
+<style>
+body{
+    height: 150vh;
+}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,51 @@
+<style src="/src/assets/style-navbar.css"></style>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<script>
+export default {
+    data: function () {
+        return {
+            isActive: false,
+            isNotActive: true
+        }
+    }
+}
+</script>
+
 <template>
-    <div class="navbar">
-        Navbar
-    </div>
+    <header>
+        <div class="menu-wrapper">
+            <div class="fries-button menu-trigger">
+                <label for="fries">
+                    <input type="checkbox" id="fries">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </label>
+            </div>
+        </div>
+    </header>
 </template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <div class="bento" :class="{active: isActive},{is_not_active: isNotActive}" @click="isActive = !isActive; isNotActive = !isNotActive">
+                
+</div> -->
